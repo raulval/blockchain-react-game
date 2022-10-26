3382eaa7dbc95b0eef80edb037e9c6e68fbe554e
Raul Val <raulval@outlook.com>
2022-10-26 17:34:10 -0300

    feat: create game component

---
+++ src/components/Game/index.tsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import database from "services/database.json";
+import { correctAnswer, incorrectAnswer } from "services/Web3Client";
+
+export interface Database {
+  questions: Question[];
+}
+
+export interface Question {
+  id: number;
+  question: string;
+  answers: Answer[];
+}
+
+export interface Answer {
+  answer: string;
+  correct: boolean;
+}
+
+type GameProps = {
+  onEndGame: (score: number, totalQuestions: number) => void;
+  updatePlayerBalance: () => void;
+};
+
+const Game = ({ onEndGame, updatePlayerBalance }: GameProps) => {
+  const [questionsData, setQuestionsData] = useState<Database>(database);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState<number>();
+  const [score, setScore] = useState(0);
+
+  const handleConfirm = () => {
+    if (selectedAnswer) {
+      const isCorrect =
+        questionsData.questions[currentQuestion].answers[selectedAnswer]
+          .correct;
+
+      if (isCorrect) {
+        correctAnswer().then(() => {
+          updatePlayerBalance();
+          setScore(score + 1);
+          if (currentQuestion + 1 < questionsData.questions.length) {
+            setSelectedAnswer(undefined);
+            setCurrentQuestion(currentQuestion + 1);
+          } else {
+            onEndGame(score, questionsData.questions.length);
+          }
+        });
+      } else {
+        incorrectAnswer().then(() => {
+          updatePlayerBalance();
+          if (currentQuestion + 1 < questionsData.questions.length) {
+            setSelectedAnswer(undefined);
+            setCurrentQuestion(currentQuestion + 1);
+          } else {
+            onEndGame(score, questionsData.questions.length);
+          }
+        });
+      }
+    }
+  };
+
+  return (
+    <div className="container flex flex-col justify-center items-center mx-auto mb-3">
+      <h1 className="title text-center text-2xl font-bold mb-6">
+        {questionsData.questions[currentQuestion].question}
+      </h1>
+      <div className="btn-group btn-group-vertical w-1/2">
+        {questionsData.questions[currentQuestion].answers.map(
+          (answer: Answer, i) => {
+            const handleSelectAnswer = () => {
+              setSelectedAnswer(i);
+            };
+            console.log("Selected Answer", selectedAnswer);
+
+            return (
+              <input
+                key={answer.answer}
+                type="radio"
+                id="option0"
+                name="answer"
+                data-title={answer.answer}
+                className="btn mt-3"
+                onClick={handleSelectAnswer}
+              />
+            );
+          }
+        )}
+      </div>
+      <button
+        className="btn btn-info mt-5 w-1/2 hover:bg-sky-600"
+        onClick={handleConfirm}
+        disabled={!selectedAnswer}
+      >
+        Confirm answer
+      </button>
+    </div>
+  );
+};
+
+export default Game;
